0224d1e6d9de23a5b74245f8ce91e4ef6c269d41
daniyal ahmed <111660699+danizus@users.noreply.github.com>
2023-08-20 16:14:25 +0500

    bubblesort added

---
+++ recursion/bubbleSortByRecursion.java
@@ -0,0 +1,44 @@
+
+import java.util.*;
+
+
+// bubbleSort Using Recursion
+public class bubbleSortByRecursion {
+	
+	public static void bubbleSortUsingRecursion(int array[],int size){
+		//base case - already sorted
+		if(size == 0 || size==1) {
+			return ;
+		}
+		
+		//recursive case
+		for(int i=0;i<size-1;i++) {
+			if(array[i]>array[i+1]) {
+				int temp = array[i];
+				array[i]= array[i+1];
+				array[i+1] = temp;
+			}
+		}
+		
+		bubbleSortUsingRecursion(array,size-1);
+
+		
+		
+		
+		
+		
+		}
+	public static void main(String[] args) {
+		
+		int array[] = {10,3,4,2,5};
+		int size= array.length;
+		bubbleSortUsingRecursion(array,size);
+		for(int i=0;i<size;i++) {
+			System.out.print(array[i]+ " "); 
+		}
+		
+		 
+		
+	}
+}
+
