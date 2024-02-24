9da00bf3208e302f0bf46581e8015ca00a7bf015
daniyal ahmed <111660699+danizus@users.noreply.github.com>
2024-02-24 21:12:21 +0500

    roman to integer added

---
+++ leetcode questions/RomanToInteger.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+import java.io.*;
+
+	public class RomanToInteger {
+		 int ans = 0;	
+		 public int romanToInt(String s) {
+			
+			 HashMap<Character, Integer> m = new HashMap<Character, Integer>();
+			 m.put('I', 1);
+			 m.put('V', 5);
+			 m.put('X', 10);
+			 m.put('L', 50);
+			 m.put('C', 100);
+			 m.put('D', 500);
+			 m.put('M', 1000);
+			 for(int i=0;i<s.length();i++) {
+				 if(i==s.length()-1){
+					 ans+=m.get(s.charAt(i));
+				 }
+				 else {
+					 if(m.get(s.charAt(i))<m.get(s.charAt(i+1))) {
+						 ans-=m.get(s.charAt(i));
+						 
+					 }else if(m.get(s.charAt(i))>m.get(s.charAt(i+1))) {
+						 ans+=m.get(s.charAt(i));
+					 }else {
+						 ans+=m.get(s.charAt(i));
+					 }
+				 }
+			 }
+			 return ans;
+			 
+		        
+		    }
+		 public static void main(String[] args){
+	        RomanToInteger objectName = new RomanToInteger(); 
+	       System.out.println((objectName.romanToInt("IX")));
+	    }
+	}
+	   
+	    
